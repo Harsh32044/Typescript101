0df14a7473a5b3f573d3dbdfb7ffb18e48f101fb
Harsh Srivastav <hsrivastav45@gmail.com>
2023-05-08 23:54:35 +0530

    Inheritance in Interfaces

--- myInterface.ts
+++ myInterface.ts
@@ -7,12 +7,36 @@ interface User{
     startTrail():string,
     getCoupon(coupName: string, off?:number): number
 }
+
+interface User {
+    githubToken: string
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
 //Loose form of classes
 
 const harsh: User = {
     dbId: 23234352,
     email:"hsr@gm.com",
     userId: 23454,
+    githubToken:"HYGEBugasdyi655768y3",
+
+    startTrail: () => {
+        return ''},
+
+    getCoupon: (name:"Harsh", offer: 1) => {
+        return 11
+    }
+}
+
+const newHarsh: Admin = {
+    dbId: 23234352,
+    email:"hsr@gm.com",
+    userId: 23454,
+    githubToken:"HYGEBugasdyi655768y3",
+    role: "admin",
 
     startTrail: () => {
         return ''},
